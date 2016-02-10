4f99b33c247c5b005c848b69457f8a68b043b07c
Sam Cohen <samc1213@gmail.com>
2016-02-10 00:27:06 -0600

    made everything a dictionary

--- gamesteamsimportwithoutfcs.py
+++ gamesteamsimportwithoutfcs.py
@@ -7,42 +7,54 @@ Therefore, some long-run summing might get messed up.
 import csv
 import datetime
 
+def GetMaxMins(attribute, games):
+    combined = []
+    for key, game in games.iteritems():
+        combined.append(game[attribute][0])
+        combined.append(game[attribute][1])
+    return max(combined), min(combined)
+
+def GetAverage(attribute, games):
+    return sum([g[attribute][0] + g[attribute][1] for key, g in games.iteritems()])/(len(games) * 2)
+
 class Team:
     def __init__(self, name=None, games=None):
         if games is None:
             self.games = []
         self.name = name
 
-class Game:
-    def __init__(self, date, gamecode, hometeamcode, visitteamcode, rushyds=None, passyds=None, rushtds=None, passtds=None, points=None, avgoffrushyds=None, avgdefrushyds=None, avgoffpassyds=None, avgdefpassyds=None, passats=None, rushats=None,  yardsperplay=None):
-        self.date = date
-        self.hometeamcode = hometeamcode
-        self.visitteamcode = visitteamcode
-        self.gamecode = gamecode
-        if rushyds is None:
-            self.rushyds = [None, None]
-        if passyds is None:
-            self.passyds = [None, None]
-        if points is None:
-            self.points = [None, None]
-        if rushtds is None:
-            self.rushtds = [None, None]
-        if passtds is None:
-            self.passtds = [None, None]
-        if avgoffrushyds is None:
-            self.avgoffrushyds = [None, None]
-        if avgdefrushyds is None:
-            self.avgdefrushyds = [None, None]
-        if avgdefpassyds is None:
-            self.avgdefpassyds = [None, None]
-        if avgoffpassyds is None:
-            self.avgoffpassyds = [None, None]
-        if passats is None:
-            self.passats = [None, None]
-        if rushats is None:
-            self.rushats = [None, None]
-        if yardsperplay is None:
-            self.yardsperplay = [None, None]
+# class Game:
+#     def __init__(self, date, gamecode, hometeamcode, visitteamcode, rushyds=None, passyds=None, rushtds=None, passtds=None, points=None, avgoffrushyds=None, avgdefrushyds=None, avgoffpassyds=None, avgdefpassyds=None, passats=None, rushats=None,  yardsperplay=None):
+#         self.date = date
+#         self.hometeamcode = hometeamcode
+#         self.visitteamcode = visitteamcode
+#         self.gamecode = gamecode
+#         if rushyds is None:
+#             self.rushyds = [None, None]
+#         if passyds is None:
+#             self.passyds = [None, None]
+#         if points is None:
+#             self.points = [None, None]
+#         if rushtds is None:
+#             self.rushtds = [None, None]
+#         if passtds is None:
+#             self.passtds = [None, None]
+#         if avgoffrushyds is None:
+#             self.avgoffrushyds = [None, None]
+#         if avgdefrushyds is None:
+#             self.avgdefrushyds = [None, None]
+#         if avgdefpassyds is None:
+#             self.avgdefpassyds = [None, None]
+#         if avgoffpassyds is None:
+#             self.avgoffpassyds = [None, None]
+#         if passats is None:
+#             self.passats = [None, None]
+#         if rushats is None:
+#             self.rushats = [None, None]
+#         if yardsperplay is None:
+#             self.yardsperplay = [None, None]
+    # def getAttr(self, str):
+    #     return self[str]
 
 def getMeTeamsAndGamesBitch(badteamsout=True):
     teams = {}
@@ -55,38 +67,55 @@ def getMeTeamsAndGamesBitch(badteamsout=True):
             gamecode = int(row['Game Code'])
             gamedate = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').date()
 
-            games[gamecode] = Game(gamedate, gamecode, hometeamcode, visitteamcode)
+            newgame = {}
+            newgame['date'] = gamedate
+            newgame['gamecode'] = gamecode
+            newgame['hometeamcode'] = hometeamcode
+            newgame['visitteamcode'] = visitteamcode
+            newgame['rushyds'] = [None, None]
+            newgame['passyds'] = [None, None]
+            newgame['rushtds'] = [None, None]
+            newgame['passtds'] = [None, None]
+            newgame['passats'] = [None, None]
+            newgame['rushats'] = [None, None]
+            newgame['points'] = [None, None]
+            newgame['avgoffrushyds'] = [None, None]
+            newgame['avgdefrushyds'] = [None, None]
+            newgame['avgoffpassyds'] = [None, None]
+            newgame['avgdefpassyds'] = [None, None]
+            newgame['yardsperplay'] = [None, None]
+            games[gamecode] = newgame
 
     with open('data/2013/team-game-statistics.csv') as gamestats:
         for row in csv.DictReader(gamestats):
             gamecode = int(row['Game Code'])
             rowTeamCode = int(row['Team Code'])
             rowGame = games[gamecode]
-            rowGame.gamecode = gamecode
+            # rowGame['gamecode'] = gamecode
             # HOME, AWAY!!!! HOME, AWAY in ze lists
-            if rowGame.hometeamcode == rowTeamCode:
-                rowGame.rushyds[0] = int(row['Rush Yard'])
-                rowGame.passyds[0] = int(row['Pass Yard'])
-                rowGame.rushtds[0] = int(row['Rush TD'])
-                rowGame.passtds[0] = int(row['Pass TD'])
-                rowGame.passats[0] = int(row['Pass Att'])
-                rowGame.rushats[0] = int(row['Rush Att'])
-                rowGame.points[0] = int(row['Points'])
+            if rowGame['hometeamcode'] == rowTeamCode:
+                rowGame['rushyds'][0] = int(row['Rush Yard'])
+                rowGame['passyds'][0] = int(row['Pass Yard'])
+                rowGame['rushtds'][0] = int(row['Rush TD'])
+                rowGame['passtds'][0] = int(row['Pass TD'])
+                rowGame['passats'][0] = int(row['Pass Att'])
+                rowGame['rushats'][0] = int(row['Rush Att'])
+                rowGame['points'][0] = int(row['Points'])
             else:
-                rowGame.rushyds[1] = int(row['Rush Yard'])
-                rowGame.passyds[1] = int(row['Pass Yard'])
-                rowGame.rushtds[1] = int(row['Rush TD'])
-                rowGame.passtds[1] = int(row['Pass TD'])
-                rowGame.passats[1] = int(row['Pass Att'])
-                rowGame.rushats[1] = int(row['Rush Att'])
-                rowGame.points[1] = int(row['Points'])
+                rowGame['rushyds'][1] = int(row['Rush Yard'])
+                rowGame['passyds'][1] = int(row['Pass Yard'])
+                rowGame['rushtds'][1] = int(row['Rush TD'])
+                rowGame['passtds'][1] = int(row['Pass TD'])
+                rowGame['passats'][1] = int(row['Pass Att'])
+                rowGame['rushats'][1] = int(row['Rush Att'])
+                rowGame['points'][1] = int(row['Points'])
             if rowTeamCode not in teams:
                 teams[rowTeamCode] = Team()
 
             teams[rowTeamCode].games.append(gamecode)
 
     for code, team in teams.iteritems():
-        team.games.sort(key=lambda x: games[x].date)
+        team.games.sort(key=lambda x: games[x]['date'])
 
     badteams = []
     badgames = []
@@ -100,8 +129,8 @@ def getMeTeamsAndGamesBitch(badteamsout=True):
                     badgames.append(g)
     for g in badgames:
 
-        homecode = games[g].hometeamcode
-        visitcode = games[g].visitteamcode
+        homecode = games[g]['hometeamcode']
+        visitcode = games[g]['visitteamcode']
         if (g == 317000520130907):
             print homecode
             print visitcode
@@ -133,39 +162,39 @@ def getMeTeamsAndGamesBitch(badteamsout=True):
         print teamid
         for g in range(len(team.games)):
             gamecode = team.games[g]
-            if games[gamecode].hometeamcode == teamid:
+            if games[gamecode]['hometeamcode'] == teamid:
                 A = 0
                 B = 1
-            elif games[gamecode].visitteamcode == teamid:
+            elif games[gamecode]['visitteamcode'] == teamid:
                 A = 1
                 B = 0
             else:
                 print "ERROR"
             if firstgame:
-                games[gamecode].avgoffrushyds[A] = offrushyds
-                games[gamecode].avgdefrushyds[A] = defrushyds
-                games[gamecode].avgoffpassyds[A] = offpassyds
-                games[gamecode].avgdefpassyds[A] = defpassyds
-                games[gamecode].yardsperplay[A] = 0
+                games[gamecode]['avgoffrushyds'][A] = offrushyds
+                games[gamecode]['avgdefrushyds'][A] = defrushyds
+                games[gamecode]['avgoffpassyds'][A] = offpassyds
+                games[gamecode]['avgdefpassyds'][A] = defpassyds
+                games[gamecode]['yardsperplay'][A] = 0
                 firstgame = False
-                offrushyds += games[gamecode].rushyds[A]
-                defrushyds += games[gamecode].rushyds[B]
-                offpassyds += games[gamecode].passyds[A]
-                defpassyds += games[gamecode].passyds[B]
-                rushats += games[gamecode].rushats[A]
-                passats += games[gamecode].passats[A]
+                offrushyds += games[gamecode]['rushyds'][A]
+                defrushyds += games[gamecode]['rushyds'][B]
+                offpassyds += games[gamecode]['passyds'][A]
+                defpassyds += games[gamecode]['passyds'][B]
+                rushats += games[gamecode]['rushats'][A]
+                passats += games[gamecode]['passats'][A]
             else:
-                games[gamecode].avgoffrushyds[A] = offrushyds/gamecount
-                games[gamecode].avgdefrushyds[A] = defrushyds/gamecount
-                games[gamecode].avgoffpassyds[A] = offpassyds/gamecount
-                games[gamecode].avgdefpassyds[A] = defpassyds/gamecount
-                games[gamecode].yardsperplay[A] = (offrushyds + offpassyds) / (rushats + passats)
-                offrushyds += games[gamecode].rushyds[A]
-                defrushyds += games[gamecode].rushyds[B]
-                offpassyds += games[gamecode].passyds[A]
-                defpassyds += games[gamecode].passyds[B]
-                rushats += games[gamecode].rushats[A]
-                passats += games[gamecode].passats[A]
+                games[gamecode]['avgoffrushyds'][A] = offrushyds/gamecount
+                games[gamecode]['avgdefrushyds'][A] = defrushyds/gamecount
+                games[gamecode]['avgoffpassyds'][A] = offpassyds/gamecount
+                games[gamecode]['avgdefpassyds'][A] = defpassyds/gamecount
+                games[gamecode]['yardsperplay'][A] = (offrushyds + offpassyds) / (rushats + passats)
+                offrushyds += games[gamecode]['rushyds'][A]
+                defrushyds += games[gamecode]['rushyds'][B]
+                offpassyds += games[gamecode]['passyds'][A]
+                defpassyds += games[gamecode]['passyds'][B]
+                rushats += games[gamecode]['rushats'][A]
+                passats += games[gamecode]['passats'][A]
             gamecount += 1.0
     firstgames = []
     for code, team in teams.iteritems():
@@ -174,8 +203,15 @@ def getMeTeamsAndGamesBitch(badteamsout=True):
             del team.games[0]
     for g in firstgames:
         del games[g]
-    print games[teams[5].games[0]].avgoffrushyds
-    return teams, games
-teams, games = getMeTeamsAndGamesBitch()
-print "teams", len(teams)
-print "games", len(games)
+
+    metadata = {}
+    metadata['maxmins'] = {}
+    metadata['averages'] = {}
+    metadata['maxmins']['avgoffrushyds'] = GetMaxMins('avgoffrushyds', games)
+    metadata['maxmins']['avgdefrushyds'] = GetMaxMins('avgdefrushyds', games)
+    metadata['maxmins']['avgoffpassyds'] = GetMaxMins('avgoffpassyds', games)
+    metadata['maxmins']['avgdefpassyds'] = GetMaxMins('avgdefpassyds', games)
+    metadata['maxmins']['yardsperplay'] = GetMaxMins('yardsperplay', games)
+    metadata['averages']['points'] = GetAverage('points', games)
+
+    return teams, games, metadata
--- testnetwork.py
+++ testnetwork.py
@@ -4,9 +4,12 @@ from pybrain.utilities           import percentError
 from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules   import SoftmaxLayer, LinearLayer, SigmoidLayer, TanhLayer
-from testing import getMeTeamsAndGamesBitch
+from gamesteamsimportwithoutfcs import getMeTeamsAndGamesBitch
 
-
+def Normalize(minmaxtuple, value):
+    newvalues = []
+    denom = minmaxtuple[1] - minmaxtuple[0]
+    return (value - minmaxtuple[0]) / denom
 
 n = FeedForwardNetwork()
 inLayer = LinearLayer(10)
@@ -27,18 +30,15 @@ n.addConnection(hidden_to_out)
 
 n.sortModules()
 
-
-
-
-teams, games = getMeTeamsAndGamesBitch()
+teams, games, metadata = getMeTeamsAndGamesBitch()
 alldata = SupervisedDataSet(10, 1)
 # alldata = ClassificationDataSet(10)
 
 #home, away
 for gameid, game in games.iteritems():
-    inputs = [game.avgoffrushyds[0], game.avgoffrushyds[1], game.avgoffpassyds[0], game.avgoffpassyds[1], game.avgdefrushyds[0], game.avgdefrushyds[1], game.avgdefpassyds[0], game.avgdefpassyds[1], game.yardsperplay[0], game.yardsperplay[1]]
+    inputs = [game['avgoffrushyds'][0], game['avgoffrushyds'][1], game['avgoffpassyds'][0], game['avgoffpassyds'][1], game['avgdefrushyds'][0], game['avgdefrushyds'][1], game['avgdefpassyds'][0], game['avgdefpassyds'][1], game['yardsperplay'][0], game['yardsperplay'][1]]
     # inputs = [game.yardsperplay[0], game.yardsperplay[1]]
-    outputs = [(game.points[0] - game.points[1])]
+    outputs = [(game['points'][0] - game['points'][1])]
     alldata.addSample(inputs, outputs)
 
 testdata, traindata = alldata.splitWithProportion(0.25)
@@ -51,10 +51,21 @@ totalcount = 0
 rightcount = 0
 for gameid, game in games.iteritems():
     totalcount+=1
-    inputs = [game.avgoffrushyds[0], game.avgoffrushyds[1], game.avgoffpassyds[0], game.avgoffpassyds[1], game.avgdefrushyds[0], game.avgdefrushyds[1], game.avgdefpassyds[0], game.avgdefpassyds[1], game.yardsperplay[0], game.yardsperplay[1]]
+    normavgoffrushydshome = Normalize(metadata['maxmins']['avgoffrushyds'], game['avgoffrushyds'][0])
+    normavgoffrushydsaway = Normalize(metadata['maxmins']['avgoffrushyds'], game['avgoffrushyds'][1])
+    normavgoffpassydshome = Normalize(metadata['maxmins']['avgoffpassyds'], game['avgoffpassyds'][0])
+    normavgoffpassydsaway = Normalize(metadata['maxmins']['avgoffpassyds'], game['avgoffpassyds'][1])
+    normavgdefrushydshome = Normalize(metadata['maxmins']['avgdefrushyds'], game['avgdefrushyds'][0])
+    normavgdefrushydsaway = Normalize(metadata['maxmins']['avgdefrushyds'], game['avgdefrushyds'][1])
+    normavgdefpassydshome = Normalize(metadata['maxmins']['avgdefpassyds'], game['avgdefpassyds'][0])
+    normavgdefpassydsaway = Normalize(metadata['maxmins']['avgdefpassyds'], game['avgdefpassyds'][1])
+    normyardsperplayhome = Normalize(metadata['maxmins']['yardsperplay'], game['yardsperplay'][0])
+    normyardsperplayaway = Normalize(metadata['maxmins']['yardsperplay'], game['yardsperplay'][1])
+
+    inputs = [normavgoffrushydshome, normavgoffrushydsaway, normavgoffpassydshome, normavgoffpassydsaway, normavgdefrushydshome, normavgdefrushydsaway, normavgdefpassydshome, normavgdefpassydsaway, normyardsperplayhome, normyardsperplayaway]
     expectedOutput = n.activate(inputs)
     print expectedOutput
-    print game.points[0]-game.points[1]
+    # print game.points[0]-game.points[1]
     if ((game.points[0] - game.points[1])>0) == expectedOutput:
         rightcount += 1
-    print rightcount / float(totalcount)
+    # print rightcount / float(totalcount)
